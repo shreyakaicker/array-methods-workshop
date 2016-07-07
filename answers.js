2ad3a2b673a27ba40e4101b52fafe716e4ec93cc
Shreya Kaicker <shreyakaicker@gmail.com>
2016-07-07 19:44:41 +0000

    phewww

--- answers.js
+++ answers.js
@@ -38,3 +38,21 @@ console.log(arrayNum2) ;
 getPositives([1,-1,2,-2,3])
 
 
+
+// Exercise 2B
+
+/*Re-do exercise 1 by first filtering the input array, and then 
+printing the numbers from the filtered array. Your code will look 
+something like: `return arr.filter(…).forEach(…) */
+
+
+function printPositives(array) {
+    
+array.filter(function(num) { if(num > 0){return true;}}).forEach(function(item) {
+          
+             console.log(item);
+         
+     })
+ }
+ 
+ printPositives([1, -2, 3, -4, 5, -6, 7, 8]);
