5453b835c9f5eb657eef6fa2956965a5ffc090f3
Shreya Kaicker <shreyakaicker@gmail.com>
2016-07-07 18:32:58 +0000

    first qs

--- answers.js
+++ answers.js
@@ -1,1 +1,20 @@
-//
+/*Exercise 1
+
+Write a function called printPositives that takes an array and 
+uses the forEach method to print only the positive numbers.*/
+
+
+var arrayNum = [1,-1,2,-2,3] ;
+var arrayNum2 = [ ] ;
+
+
+
+arrayNum.forEach(function(number) { 
+   if(number > 0){
+       arrayNum2.push(number)
+   }
+})
+
+
+console.log(arrayNum2) ;
+
